a441426f5d85a657991df58dfda93c4ac428fd42
Arthur Mariz <85581944+arthursm-aqb@users.noreply.github.com>
2026-06-23 14:51:25 +0000

    tabela paciente criada e finalizada

--- exercicios/consultas_ddl.sql
+++ exercicios/consultas_ddl.sql
@@ -1,10 +1,21 @@
 CREATE SCHEMA dbagconsultas;
 
-CREATE TABLE IF NOT EXISTS dbagconsultas.paciente(
+CREATE TABLE IF NOT EXISTS dbagconsultas.pessoa(
     cpf char(11) PRIMARY KEY,
     email varchar(50) NOT NULL,
     nome varchar(150) NOT NULL,
     data_nasc DATE NOT NULL,
     endereco varchar(300) NOT NULL,
-    telefone varchar(15) NULL 
+    telefone varchar(15) NULL,
+
+    CONSTRAINT unique_1 UNIQUE(email, nome)
 );
+
+CREATE TABLE IF NOT EXISTS dbagconsultas.paciente(
+    cpf_paciente char(11) PRIMARY KEY,
+    senha varchar(20) NOT NULL,
+    plano_saude boolean NOT NULL DEFAULT FALSE,
+
+    CONSTRAINT cpf_paciente_fk FOREIGN KEY (cpf_paciente) REFERENCES dbagconsultas.pessoa(cpf)
+);
+
