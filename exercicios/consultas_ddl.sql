0edf957dbe5691cc69cb730cba27cbcfa036ae21
Arthur Mariz <arthur.mariz_silva@hotmail.com>
2026-06-23 21:38:22 -0300

    todas tabelas criadas

--- exercicios/consultas_ddl.sql
+++ exercicios/consultas_ddl.sql
@@ -16,12 +16,36 @@ CREATE TABLE IF NOT EXISTS dbagconsultas.paciente(
     senha varchar(20) NOT NULL,
     plano_saude boolean NOT NULL DEFAULT FALSE,
 
-    CONSTRAINT cpf_paciente_fk FOREIGN KEY (cpf_paciente) REFERENCES dbagconsultas.pessoa(cpf)
+    CONSTRAINT cpf_paciente_fk1 FOREIGN KEY (cpf_paciente) REFERENCES dbagconsultas.pessoa(cpf)
 );
 
 CREATE TABLE IF NOT EXISTS dbagconsultas.medico(
     cpf_medico char(11) PRIMARY KEY,
     crm varchar(10) UNIQUE NOT NULL,
 
-    CONSTRAINT cpf_medico_fk FOREIGN KEY(cpf_medico) REFERENCES dbagconsultas.pessoa(cpf) 
+    CONSTRAINT cpf_medico_fk1 FOREIGN KEY(cpf_medico) REFERENCES dbagconsultas.pessoa(cpf) 
+);
+
+CREATE TABLE IF NOT EXISTS dbagconsultas.agendamento(
+    cpf_paciente char(11) PRIMARY KEY,
+    cpf_medico char(11) PRIMARY KEY,
+    dh_consulta TIMESTAMP PRIMARY KEY,
+    dh_agendamento TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    valor_consulta float NOT NULL DEFAULT 0.0,
+
+    CONSTRAINT cpf_paciente_fk2 FOREIGN KEY(cpf_paciente) REFERENCES dbagconsultas.paciente(cpf_paciente),
+    CONSTRAINT cpf_medico_fk2 FOREIGN KEY(cpf_medico) REFERENCES dbagconsultas.medico(cpf_medico)
+);
+
+CREATE TABLE IF NOT EXISTS dbagconsultas.especialidade(
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    descricao varchar(300) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS dbagconsultas.MedicoEspecialidade(
+    cpf_medico char(11) PRIMARY KEY,
+    id_especialidade int PRIMARY KEY,
+
+    CONSTRAINT cpf_medico_fk3 FOREIGN KEY(cpf_medico) REFERENCES dbagconsultas.medico(cpf_medico),
+    CONSTRAINT id_especialidade_fk FOREIGN KEY(id_especialidade) REFERENCES dbagconsultas.especialidade(id)
 );
